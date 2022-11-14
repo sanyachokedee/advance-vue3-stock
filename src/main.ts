bdf6351843c729fbaa6bf6bec804bf1e961d6b1e
Nutthakit Anansuksakul <ing118199@gmail.com>
2022-11-14 18:15:57 +0700

    done

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+
+// Import Tailwind css and Customer.css file
+import './tailwind/app.css'
+import './tailwind/custom.css'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
